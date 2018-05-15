20d0a64a14e472307bf1d466cc4747725b6e908f
Steven Woodhouse <steven.j.woodhouse@gmail.com>
2018-05-15 11:28:20 +0100

    First attempt at increasingReachability, still need to write checkTransition

--- src/BioCheckAnalyzer/IncreasingReachability.fs
+++ src/BioCheckAnalyzer/IncreasingReachability.fs
@@ -3,15 +3,70 @@
 
 module IncreasingReachability
 
+(*
+initial ranges, per sub-model. or attractor per sub-model [per unmutated sub-model]
+
+1. -1 +1 each side of range of each var
+2. For each possible concrete val of each var, and each abstract range of other vars, can i rule out some element of range in successor.
+   do as a list not min/max bound. order of choice of variable does not matter. left to right
+3. stopping condition is that ranges stop changing
+4. one level back. global stopping condition is that ranges converge
+if SAT then ...
+
+not so straightforward to implement this...
+
+*)
+
+let keys = Map.toList >> List.unzip >> fst
+
+let expandRange (qn : QN.node list) v r =
+    let x = qn |> List.find (fun n -> n.var = v)
+    let min, max = x.range
+    let r = if List.head r <> min then (List.head r - 1) :: r else r
+    let r = if List.last r <> max then r @ [List.last r + 1] else r
+    r
+
+let checkTransition _ _ _ = // temp
+    failwith "unimplemented"
+
+let remove x range = List.filter ((<>) x) range
+
+// write imperative to begin with. i think a mix of loop and recursion would work..
+// need a z3 context somewhere too..
+// check with nir that this seems correct
+let rec increasingReachability (qn : QN.node list) (initialRanges : Map<QN.var, int list>) =
+  let mutable expandedRanges = Map.empty
+  let mutable newRanges = Map.map (expandRange qn) initialRanges
+
+  // wait.. do i really need a loop and recursion.. yeah.. because next round you re-expand.. except expand is wrong if i do like this, with potential gaps in the range.. right?
+  // while loop. expandedNew != expanded // can we do with a generator expression instead?
+  while expandedRanges <> newRanges do
+    expandedRanges <- newRanges
+    for var in keys newRanges do
+      let range = Map.find var newRanges
+      for x in range do
+        if not (checkTransition qn x newRanges) then
+          newRanges <- Map.add var (remove x range) newRanges
+
+  if expandedRanges = initialRanges then [initialRanges] // or []?
+  else initialRanges :: increasingReachability qn expandedRanges // or expandedRanges :: ?
+
+
+// move this to Game.fs
+// let runIncreasingReachability qn initialRanges =
+//     increasingReachability qn initialRanges |> Attractors.collapseRanges // not sure if this is right..
+  
+// stepZ3rangelist.find_paths : qn -> int -> Map<QN.var, int list> -> Map<QN.var, int list> -> Map<QN.var, int list>
+// why two range parameters?
+
+// assert_query : qn.node -> value:int -> time:int -> z -> s -> c
+// assert_target_function : ...
+// BioCheckZ3.assert_bound : qn.node -> lower:int -> upper:int -> time:int -> z -> s -> c
+
 // open Paths // not sure if we can directly use this
 // open stepZ3rangelist
 
 // do backwards reachability until convergence to all possible states.. at the level of ranges.. return the seq
-let increasingReachability (qn : QN.node list) initialBounds =
-    // seems difficult to implement
-    // 
-
-    failwith "unimplemented"
 // what is the sig for this.. do i do for each submodel or the mega model, and do i do from states or ranges. i guess from a range. then i need to intersect
 // can i call find_paths or do i need to reimplement something like that
 
